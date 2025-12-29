35f383698fbe89572fb5c7bdeb3b2f8bc2d8b6fb
ulicortes <ulisestcortes@gmail.com>
2025-12-29 19:13:27 -0300

    Cree la app de angular exitosamente.

---
+++ src/app/stock/stock.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock',
+  imports: [],
+  templateUrl: './stock.html',
+  styleUrl: './stock.css',
+})
+export class Stock {
+
+}
